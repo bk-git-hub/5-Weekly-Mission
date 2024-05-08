5a20445988e5ecf5e84dc3c9c29bccfd1c631ca0
bk-git-hub <bkprivate99@gmail.com>
2024-05-08 17:13:05 +0900

    chore: Change Logo image to Link Component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,14 +1,15 @@
 import Button from '../Button/Button';
 import Image from 'next/image';
 import styles from './Header.module.css';
+import Link from 'next/link';
 export default function Header() {
   return (
     <>
       <header className={styles.headerContainer}>
         <div className={styles.headerBar}>
-          <div className={styles.logoImageContainer}>
+          <Link className={styles.logoImageContainer} href={`/`}>
             <Image src='/assets/images/logo.svg' alt='Logo' fill />
-          </div>
+          </Link>
           <Button>{'로그인'}</Button>
         </div>
       </header>
