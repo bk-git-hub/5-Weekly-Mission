54a6d8653c03696a8b2d006c2111c2fffb4f7ae0
bk-git-hub <bkprivate99@gmail.com>
2024-05-09 13:50:23 +0900

    feat: Add optional rendering of header and footer in app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,19 @@ import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const isAuthPage = Component.name === 'SignIn' || Component.name === 'SignUp';
+
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+      {isAuthPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
