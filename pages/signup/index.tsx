9c7af942e8ad751f9fd48c58745e0892b92b9704
bk-git-hub <bkprivate99@gmail.com>
2024-05-09 16:17:56 +0900

    feat: Add social link bar in sign up page

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,6 +1,7 @@
 import styles from '@/styles/AuthPage.module.css';
 import AuthHeader from '@/components/AuthHeader/AuthHeader';
 import SignUpForm from '@/components/SignUpForm/SignUpForm';
+import SocialAuth from '@/components/SocialAuth/SocialAuth';
 
 export default function SignUp() {
   return (
@@ -9,6 +10,7 @@ export default function SignUp() {
         <div className={styles.authPageContainer}>
           <AuthHeader text='이미 회원이신가요?' linkText='로그인하기' />
           <SignUpForm />
+          <SocialAuth text='다른 방식으로 가입하기' />
         </div>
       </main>
     </>
