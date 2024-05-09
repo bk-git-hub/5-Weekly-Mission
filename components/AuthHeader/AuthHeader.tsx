abf29561b43ae740a90eeaef105da43c1210fdcd
bk-git-hub <bkprivate99@gmail.com>
2024-05-09 14:22:14 +0900

    refactor: Extract authHeader from authPage into a seperate component

---
+++ components/AuthHeader/AuthHeader.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './AuthHeader.module.css';
+
+interface AuthHeaderProps {
+  text: string;
+  linkText: string;
+}
+
+export default function AuthHeader({ text, linkText }: AuthHeaderProps) {
+  return (
+    <div className={styles.authPageHeader}>
+      <Link href={'/'} className={styles.logo}>
+        <Image src={'/assets/images/logo.svg'} fill alt='Linkbrary' />
+      </Link>
+
+      <div className={styles.membershipPromptBar}>
+        <span className={styles.membershipPromptText}>{text}</span>
+        <Link href={'/signin'} className={styles.membershipPromptLink}>
+          {linkText}
+        </Link>
+      </div>
+    </div>
+  );
+}
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,26 +1,12 @@
-import Link from 'next/link';
 import styles from '@/styles/AuthPage.module.css';
-import Image from 'next/image';
+import AuthHeader from '@/components/AuthHeader/AuthHeader';
 
 export default function SignUp() {
   return (
     <>
       <main className={styles.authPage}>
         <div className={styles.authPageContainer}>
-          <div className={styles.authPageHeader}>
-            <Link href={'/'} className={styles.logo}>
-              <Image src={'/assets/images/logo.svg'} fill alt='Linkbrary' />
-            </Link>
-
-            <div className={styles.membershipPromptBar}>
-              <span className={styles.membershipPromptText}>
-                이미 회원이신가요??
-              </span>
-              <Link href={'/signin'} className={styles.membershipPromptLink}>
-                로그인 하기
-              </Link>
-            </div>
-          </div>
+          <AuthHeader text='이미 회원이신가요?' linkText='로그인하기' />
         </div>
       </main>
     </>
