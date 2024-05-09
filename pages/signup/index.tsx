0ef3b30fdc422f1e5eee37d4018664fac2dcdd92
bk-git-hub <bkprivate99@gmail.com>
2024-05-09 14:11:59 +0900

    feat: Add header for sign up page

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,3 +1,28 @@
+import Link from 'next/link';
+import styles from '@/styles/AuthPage.module.css';
+import Image from 'next/image';
+
 export default function SignUp() {
-  return <div>회원가입</div>;
+  return (
+    <>
+      <main className={styles.authPage}>
+        <div className={styles.authPageContainer}>
+          <div className={styles.authPageHeader}>
+            <Link href={'/'} className={styles.logo}>
+              <Image src={'/assets/images/logo.svg'} fill alt='Linkbrary' />
+            </Link>
+
+            <div className={styles.membershipPromptBar}>
+              <span className={styles.membershipPromptText}>
+                이미 회원이신가요??
+              </span>
+              <Link href={'/signin'} className={styles.membershipPromptLink}>
+                로그인 하기
+              </Link>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
