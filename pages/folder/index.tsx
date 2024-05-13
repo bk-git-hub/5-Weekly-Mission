24f16bbed0f94318ac7b1ca21d301eecfc8f3aa3
bk-git-hub <bkprivate99@gmail.com>
2024-05-13 19:05:24 +0900

    feat: Create folder page

--- pages/folder/index.tsx
+++ pages/folder/index.tsx
@@ -1,11 +1,19 @@
 import { useUserInfo } from '@/contexts/UserInfoContext';
 import { axiosInstance } from '@/utils/axiosInstance';
-import { useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 export default function Folder() {
   const { userInfo, setUserInfo } = useUserInfo();
+  const [folders, setFolders] = useState();
+  const [links, setLinks] = useState();
+  const router = useRouter();
+
   const loadUser = async () => {
     const accessToken = localStorage.getItem('accessToken');
+    if (!accessToken) {
+      router.push('/signin');
+    }
     const response = await axiosInstance.get('/users', {
       headers: {
         Authorization: `Bearer ${accessToken}`,
@@ -14,9 +22,31 @@ export default function Folder() {
     setUserInfo(response.data.data[0]);
   };
 
+  const getUserFolders = async () => {
+    if (userInfo) {
+      const response = await axiosInstance.get(`/users/${userInfo.id}/folders`);
+      setFolders(response.data.data);
+    }
+  };
+
+  const getUserLinks = async (folderId?: number) => {
+    if (userInfo) {
+      const folderEndPoint = folderId ? `?folderId=${folderId}` : '';
+      const response = await axiosInstance.get(
+        `/users/${userInfo.id}/links${folderEndPoint}`
+      );
+      setLinks(response.data.data);
+    }
+  };
+
   useEffect(() => {
     loadUser();
   }, []);
+
+  useEffect(() => {
+    getUserFolders();
+    getUserLinks();
+  }, [userInfo]);
 
   return <div>FolderPage</div>;
 }
