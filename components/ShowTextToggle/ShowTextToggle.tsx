ebec153d7717db72f7814a0012b3011b597fa0e5
bk-git-hub <bkprivate99@gmail.com>
2024-05-13 13:58:32 +0900

    feat: Create ShowTextToggle component

---
+++ components/ShowTextToggle/ShowTextToggle.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+
+interface ShowTextToggleProps {
+  showText: boolean;
+  onClick: () => void;
+}
+
+export default function ShowTextToggle({
+  showText,
+  onClick,
+}: ShowTextToggleProps) {
+  return (
+    <button
+      onClick={onClick}
+      style={{ width: '16px', height: '16px', position: 'relative' }} // 객체 형태로 스타일 지정
+    >
+      {showText ? (
+        <Image src={'/assets/images/eye-on.svg'} alt='Hide password' fill />
+      ) : (
+        <Image src={'/assets/images/eye-off.svg'} alt='Show password' fill />
+      )}
+    </button>
+  );
+}
