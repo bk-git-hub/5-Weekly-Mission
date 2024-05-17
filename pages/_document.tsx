1d564990962de71463684913f0c9e0d825053261
bk-git-hub <bkprivate99@gmail.com>
2024-05-17 11:45:11 +0900

    fix: Change eslintrc and add script for Kakao Talk share

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
       <body>
+        <script
+          src='https://t1.kakaocdn.net/kakao_js_sdk/2.7.1/kakao.min.js'
+          integrity='sha384-kDljxUXHaJ9xAb2AzRd59KxjrFjzHa5TAoFQ6GbYTCAG0bjM55XohjjDT7tDDC01'
+          crossOrigin='anonymous'
+        ></script>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
