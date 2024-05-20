f6150436213f32e7890bf55596f4ee010b112721
bk-git-hub <bkprivate99@gmail.com>
2024-05-20 18:00:13 +0900

    feat: Add shared page

---
+++ components/SharedLinkCardList/SharedLinkCardList.tsx
@@ -0,0 +1,49 @@
+import { LinkObj } from '@/utils/interfaces';
+import SearchBar from '../SearchBar/SearchBar';
+import styles from '@/components/LinkCardList/LinkCardList.module.scss';
+import { useState } from 'react';
+import SharedLinkCard from '../SharedLinkCard/SharedLinkCard';
+interface SharedLinkCardListProps {
+  items: LinkObj[] | undefined;
+}
+
+export default function SharedLinkCardList({ items }: SharedLinkCardListProps) {
+  const [searchText, setSearchText] = useState<string>('');
+
+  const handleSearchInput = (text: string) => {
+    setSearchText(text);
+  };
+
+  function filterLinksByKeyword(keyword: string) {
+    if (!items) return;
+    if (searchText === '') return items;
+    return items.filter(
+      (item) =>
+        item.url?.includes(keyword) ||
+        item.description?.includes(keyword) ||
+        item.title?.includes(keyword)
+    );
+  }
+
+  const curItems = filterLinksByKeyword(searchText);
+
+  return (
+    <div className={styles.linkCardListContainer}>
+      <div className={styles.contentWrapper}>
+        <SearchBar onChange={handleSearchInput} searchText={searchText} />
+
+        {curItems && curItems.length > 0 ? (
+          <ul className={styles.linkCardList}>
+            {curItems.map((item) => (
+              <li key={item.id}>
+                <SharedLinkCard linkCardInfo={item} />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className={styles.noSavedLink}>저장된 링크가 없습니다</p>
+        )}
+      </div>
+    </div>
+  );
+}
--- pages/shared/[folderId].tsx
+++ pages/shared/[folderId].tsx
@@ -1,28 +1,16 @@
 import { axiosInstance } from '@/utils/axiosInstance';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-
-interface FolderInfoObj {
-  created_at: string;
-  favorite: boolean;
-  id: number;
-  name: string;
-  user_id: number;
-}
-
-interface UserInfoObj {
-  id: number;
-  created_at: string;
-  name: string;
-  image_source: string;
-  email: string;
-}
+import { FolderInfoObj, UserInfoObj, LinkObj } from '@/utils/interfaces';
+import SharedProfile from '@/components/SharedProfile/SharedProfile';
+import SharedLinkCardList from '@/components/SharedLinkCardList/SharedLinkCardList';
 
 export default function SharedPage() {
   const router = useRouter();
   const { folderId } = router.query;
   const [folderInfo, setFolderInfo] = useState<FolderInfoObj | null>(null);
   const [userInfo, setUserInfo] = useState<UserInfoObj | null>(null);
+  const [links, setLinks] = useState<LinkObj[]>([]);
   console.log('Folder ID:', folderId);
 
   const loadSharedPage = async (folderId: any) => {
@@ -31,10 +19,28 @@ export default function SharedPage() {
     setFolderInfo(response.data.data[0]);
   };
 
+  const loadFolderInfo = async (folderInfo: FolderInfoObj | null) => {
+    if (folderInfo) {
+      getFolderOwnwer(folderInfo);
+      getUserLinks(folderInfo.id);
+    }
+  };
+
   const getFolderOwnwer = async (folderInfo: FolderInfoObj | null) => {
     if (!folderInfo) return;
     const res = axiosInstance.get(`users/${folderInfo.user_id}`);
     setUserInfo((await res).data.data[0]);
+  };
+
+  const getUserLinks = async (folderId?: number) => {
+    if (folderInfo) {
+      const folderEndPoint =
+        folderId && folderId !== -1 ? `?folderId=${folderId}` : '';
+      const response = await axiosInstance.get(
+        `/users/${folderInfo.user_id}/links${folderEndPoint}`
+      );
+      setLinks(response.data.data);
+    }
   };
 
   useEffect(() => {
@@ -42,14 +48,15 @@ export default function SharedPage() {
   }, [folderId]);
 
   useEffect(() => {
-    getFolderOwnwer(folderInfo);
+    loadFolderInfo(folderInfo);
   }, [folderInfo]);
 
   return (
     <div>
-      <h1>Shared Page</h1>
-      <p>Folder ID: {folderId}</p>
-      {folderInfo && <p>{folderInfo.user_id}</p>}
+      {folderInfo && userInfo && (
+        <SharedProfile userInfo={userInfo} folderName={folderInfo.name} />
+      )}
+      <SharedLinkCardList items={links} />
     </div>
   );
 }
