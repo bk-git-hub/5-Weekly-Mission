babdbab11e774c290da515283b39905a8cb7b891
bk-git-hub <bkprivate99@gmail.com>
2024-05-21 17:58:06 +0900

    feat: Add redirection feature to authentication pages when access token exists

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,9 +1,19 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 import styles from '@/styles/AuthPage.module.css';
 import AuthHeader from '@/components/AuthHeader/AuthHeader';
 import SignInForm from '@/components/SignInForm/SignInForm';
 import SocialAuth from '@/components/SocialAuth/SocialAuth';
 
 export default function SignUp() {
+  const router = useRouter();
+  if (localStorage.getItem('accessToken')) {
+    router.push('/folder');
+    return null;
+  }
+
   return (
     <>
       <main className={styles.authPage}>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,9 +1,20 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 import styles from '@/styles/AuthPage.module.css';
 import AuthHeader from '@/components/AuthHeader/AuthHeader';
 import SignUpForm from '@/components/SignUpForm/SignUpForm';
 import SocialAuth from '@/components/SocialAuth/SocialAuth';
 
 export default function SignUp() {
+  const router = useRouter();
+
+  if (localStorage.getItem('accessToken')) {
+    router.push('/folder');
+    return null;
+  }
+
   return (
     <>
       <main className={styles.authPage}>
