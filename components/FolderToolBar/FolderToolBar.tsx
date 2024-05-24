6050d94831fe6edb5c52e9abd44e0964ccd51a38
bk-git-hub <bkprivate99@gmail.com>
2024-05-24 17:14:37 +0900

    모달창을 툴바에 다는거까지는 성공했다잉

--- components/FolderToolBar/FolderToolBar.tsx
+++ components/FolderToolBar/FolderToolBar.tsx
@@ -5,78 +5,126 @@ import { UTIL_BUTTONS_PROPS } from './constants';
 import useFolders from './useFolders';
 import { useCurrentFolder } from '@/pages/folder/containers/useCurrentFolder';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+import Modal from '@/components/Modal/Modal';
+import useModal from './useModal';
 
 const addIcon = '/assets/images/add_icon.svg';
 const addIconWhite = '/assets/images/add_icon_white.svg';
 
 export default function FolderToolBar() {
   const { folders } = useFolders();
-  const { folderName, setFolderName } = useCurrentFolder();
+
+  const { showModal, modalContent, setShowModal, setModal } = useModal();
   const router = useRouter();
 
-  let currentFolderId = -1;
+  let currentFolderId = router.query.slug ? parseInt(router.query.slug[0]) : -1;
+  const currentFolderName = folders?.find(
+    (folder) => folder.id === currentFolderId
+  )?.name;
+  UTIL_BUTTONS_PROPS.share.onClick = () => {
+    setModal('share', {
+      headerText: '폴더 공유',
+      subHeaderText: currentFolderName,
+      folderNum: currentFolderId,
+    });
+  };
 
-  if (router.query.slug) {
-    currentFolderId = parseInt(router.query.slug[0]);
-  }
+  UTIL_BUTTONS_PROPS.changeName.onClick = () => {
+    setModal('input', {
+      headerText: '폴더 공유',
+      initialValue: currentFolderName,
+      folderNum: currentFolderId,
+      buttonText: '변경하기',
+    });
+  };
+
+  UTIL_BUTTONS_PROPS.delete.onClick = () => {
+    setModal('delete', {
+      headerText: '폴더 공유',
+      subHeaderText: currentFolderName,
+    });
+  };
 
   return (
-    <div className={styles.folderToolBarContainer}>
-      <div className={styles.folderToolButtons}>
-        <ul className={styles.folderNameButtons}>
-          {folders?.map((item) => (
-            <li key={item.id}>
-              <FolderToolBarButton
-                onClick={() => {
-                  setFolderName(item.name);
-                  if (item.id < 0) {
-                    router.push('/folder');
-                    return;
-                  }
+    <>
+      {showModal && (
+        <>
+          <Modal onClose={() => setShowModal(false)}>{modalContent}</Modal>
+          <div
+            className={styles.overlay}
+            onClick={() => setShowModal(false)}
+          ></div>
+        </>
+      )}
+      <div className={styles.folderToolBarContainer}>
+        <div className={styles.folderToolButtons}>
+          <ul className={styles.folderNameButtons}>
+            {folders?.map((item) => (
+              <li key={item.id}>
+                <FolderToolBarButton
+                  onClick={() => {
+                    if (item.id < 0) {
+                      router.push('/folder');
+                      return;
+                    }
 
-                  router.push(`/folder/${item.id}`);
-                }}
-                id={item.id}
-                isFocused={item.id == currentFolderId}
-              >
-                {item.name}
-              </FolderToolBarButton>
-            </li>
-          ))}
-        </ul>
-
-        <button className={styles.folderAddButton} onClick={() => {}}>
-          <span>폴더 추가</span>
-          <img
-            src={addIcon}
-            alt='폴더 추가 아이콘'
-            className={styles.addIcon}
-          />
-          <img
-            src={addIconWhite}
-            alt='폴더 추가 아이콘'
-            className={styles.addIconWhite}
-          />
-        </button>
-      </div>
-      <div className={styles.folderNameBar}>
-        <span className={styles.folderNameDisplay}>{folderName}</span>
-        <ul className={styles.utilButtons}>
-          {Object.entries(UTIL_BUTTONS_PROPS).map(([key, btn]) => (
-            <li key={btn.id}>
-              {currentFolderId !== -1 && (
-                <UtilButton
-                  imgSrc={btn.imgSrc}
-                  alt={btn.alt}
-                  onClick={btn.onClick}
+                    router.push(`/folder/${item.id}`);
+                  }}
+                  id={item.id}
+                  isFocused={item.id == currentFolderId}
                 >
-                  {btn.btnText}
-                </UtilButton>
-              )}
-            </li>
-          ))}
-        </ul>
+                  {item.name}
+                </FolderToolBarButton>
+              </li>
+            ))}
+          </ul>
+
+          <button
+            className={styles.folderAddButton}
+            onClick={() => {
+              setModal('input', {
+                headerText: '폴더 추가',
+                buttonText: '추가하기',
+              });
+            }}
+          >
+            <span>폴더 추가</span>
+            <Image
+              src={addIcon}
+              alt='폴더 추가 아이콘'
+              className={styles.addIcon}
+              width={16}
+              height={16}
+            />
+            <Image
+              src={addIconWhite}
+              alt='폴더 추가 아이콘'
+              className={styles.addIconWhite}
+              width={16}
+              height={16}
+            />
+          </button>
+        </div>
+        <div className={styles.folderNameBar}>
+          <span className={styles.folderNameDisplay}>{currentFolderName}</span>
+          <ul className={styles.utilButtons}>
+            {Object.entries(UTIL_BUTTONS_PROPS).map(([key, btn]) => (
+              <li key={btn.id}>
+                {currentFolderId !== -1 && (
+                  <UtilButton
+                    imgSrc={btn.imgSrc}
+                    alt={btn.alt}
+                    onClick={btn.onClick}
+                  >
+                    {btn.btnText}
+                  </UtilButton>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- pages/folder/[[...slug]].tsx
+++ pages/folder/[[...slug]].tsx
@@ -1,8 +1,6 @@
 import AddLinkBar from '@/components/AddLinkBar/AddLinkBar';
 import FolderPageContent from '@/components/FolderPageContent/FolderPageContent';
-import FolderToolBar from '@/components/FolderTo/FolderToolBar';
-import Footer from '@/components/Footer/Footer';
-import Header from '@/components/Header/Header';
+import FolderToolBar from '@/components/FolderToolBar/FolderToolBar';
 import SearchBar from '@/components/SearchBar/SearchBar';
 
 export default function Folder() {
